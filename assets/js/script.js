501abdee7c1e00dfbd9143142ded7ba81f67cdb7
zokam1411 <zokam1411@gmail.com>
2020-08-04 22:37:31 +0000

    Created function for contact form to show modal aler, reset form and disable form after submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,5 +6,15 @@ $(document).ready(function () {
   $('form[id="repairorder"]').submit(function () {
     alert("Form submitted. Thank you!");
     window.location.reload(true);
+    event.preventDefault();
+  });
+});
+
+$(document).ready(function () {
+  $('form[id="contact-form"]').submit(function () {
+    $('#success').modal("show");
+    $("form")[0].reset();
+    $("form :input").attr("disabled", "disabled");
+    event.preventDefault();
   });
 });
